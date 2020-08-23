b2255994c0487adea707e3121f77341c980fd07a
positiveKiddo@gmail.com <positiveKiddo@gmail.com>
2020-08-23 16:31:13 +0200

    release(1.0.2): @koleda/common-utils dep added

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import AuthProvider from "./context/Auth/AuthProvider";
+
+export { AuthProvider };
+export { useAuthContext } from "./context/Auth/AuthContext";
+
+export { client } from "./context/Client/ApolloClient";
