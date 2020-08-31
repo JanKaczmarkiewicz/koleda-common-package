25a161ce337f0b32c848a5935fbad4645e3703af
positiveKiddo@gmail.com <positiveKiddo@gmail.com>
2020-08-31 10:05:14 +0200

    release(2.1.2): allow subscriptions

--- src/context/Client/ApolloClient.ts
+++ src/context/Client/ApolloClient.ts
@@ -1,11 +1,20 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, split } from "@apollo/client";
 
 import { setContext } from "@apollo/client/link/context";
 import { createHttpLink } from "@apollo/react-hooks";
 import { storage } from "@koleda/common-utils";
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:3001",
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:5000/`,
+  options: {
+    reconnect: true
+  }
 });
 
 const authLink = setContext(async (_, { headers }) => {
@@ -13,12 +22,25 @@ const authLink = setContext(async (_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ?? `Bearer ${token}`,
     },
   };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: authLink.concat(splitLink),
 });
